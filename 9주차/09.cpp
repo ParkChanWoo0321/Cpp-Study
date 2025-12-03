78d6cb7b2c0652789624ca9b8b9012021d78310a
ChanWoo <op9563_@naver.com>
2025-12-03 15:06:25 +0900

    Update 09.cpp

--- 9주차/09.cpp
+++ 9주차/09.cpp
@@ -1,249 +1,249 @@
-//#include <iostream>
-//using namespace std;
-//int main(void)
-//{
-//	// string ÇüÅÂ·Î ¹è¿­ »ı¼ºÇÏ¿© ¹®ÀÚ Å½»ö
-//	string names[5];
-//	for (int i = 0; i < 5; i++)
-//	{
-//		cout << "ÀÌ¸§ >> ";
-//		getline(cin, names[i], '\n');
-//	}
-//	string first = names[0];
-//	string latter = names[0];
-//	for (int i = 1; i < 5; i++)
-//	{
-//		if (latter < names[i])
-//		{
-//			latter = names[i];
-//		}
-//		if (first > names[i])
-//		{
-//			first = names[i];
-//		}
-//	}
-//	cout << "»çÀü¿¡¼­ °¡Àå µÚ¿¡ ³ª¿À´Â ¹®ÀÚ¿­Àº " << latter << endl;
-//	cout << "»çÀü¿¡¼­ °¡Àå ¾Õ¿¡ ³ª¿À´Â ¹®ÀÚ¿­Àº " << first << endl;
-//
-//	// ¿µ¾î·Î ¹®ÀÚ¿­À» ÀÔ·Â¹Ş¾Æ ÀÚ¸®¸¦ Ã£¾Æ Ãâ·Â
-//	string s;
-//	cout << "¹®ÀÚ¿­À» ÀÔ·ÂÇÏ½Ã¿À(ÇÑ±Û ¾ÈµÊ)" << endl;
-//	getline(cin, s, '\n');
-//	int len = s.length();
-//	for (int i = 0; i < len; i++)
-//	{
-//		string first = s.substr(0, 1);
-//		string sub = s.substr(1, len - 1);
-//		s = sub + first;
-//		cout << s << endl;
-//	}
-//}
-//
-//
-//// ÇÔ¼ö Áßº¹ÇÏ¿© »ç¿ë °¡´É
-//#include <iostream>
-//using namespace std;
-//int big(int a, int b)
-//{
-//	if (a > b) return a;
-//	else return b;
-//}
-//int big(int* p, int size)
-//{
-//	int res = *p;
-//	for (int i = 1; i < size; i++)
-//		if (res < *(p + i)) res = *(p + i);
-//	return res;
-//}
-//int main(void)
-//{
-//	int array[5] = { 1,9,-2,8,6 };
-//	cout << big(2, 3) << endl;
-//	cout << big(array, 5) << endl;
-//}
-//
-//// ¿øÇÏ´Â °ªÀ» ÀÔ·Â ¾ÈÇÒ½Ã ´Ù¸¥ Á¤ÇØÁø °ªÀ¸·Î ÁöÁ¤
-//#include <iostream>
-//using namespace std;
-//void star(int a = 5)
-//{
-//	for (int i = 0; i < a; i++)
-//	{
-//		cout << "*";
-//	}
-//	cout << endl;
-//}
-//void msg(int id, string text = "")
-//{
-//	cout << id << " " << text << endl;
-//}
-//int main(void)
-//{
-//	star();
-//	star(10);
-//	msg(10);
-//	msg(10, "Hello");
-//}
-//
-//#include <iostream>
-//using namespace std;
-//void f(char c = ' ', int line = 1)
-//{
-//	for (int i = 0; i < line; i++)
-//	{
-//		for (int j = 0; j < 10; j++)
-//		{
-//			cout << c;
-//		}
-//		cout << endl;
-//	}
-//}
-//int main(void)
-//{
-//	f();
-//	f('%');
-//	f('@', 5);
-//}
-//
-//// Å¬·¡½º ¸â¹ö °ª ÁöÁ¤
-//#include <iostream>
-//using namespace std;
-//class Person
-//{
-//public:
-//	double money;
-//	void addMoney(int money)
-//	{
-//		this->money += money;
-//	}
-//	static int sharedMoney;
-//	static void addShared(int n)
-//	{
-//		sharedMoney += n;
-//	}
-//};
-//int Person::sharedMoney = 10;
-//int main(void)
-//{
-//	Person han;
-//	han.money = 100;
-//	han.sharedMoney = 200;
-//	Person lee;
-//	lee.money = 150;
-//	lee.addMoney(200);
-//	lee.addShared(200);
-//	cout << han.money << " " << lee.money << endl;
-//	cout << han.sharedMoney << " " << lee.sharedMoney << " " << Person::sharedMoney << endl;
-//}
-//
-//// ¼Ò¸êÀÚ È°¿ë
-//#include <iostream>
-//using namespace std;
-//class Circle
-//{
-//private:
-//	static int numOfCircles;
-//	int radius;
-//public:
-//	Circle(int r = 1);
-//	~Circle() { numOfCircles--; }
-//	double getArea() { return 3.14 * radius * radius; }
-//	static int getNumOfCircles() { return numOfCircles; }
-//};
-//Circle::Circle(int r)
-//{
-//	radius = r;
-//	numOfCircles++;
-//}
-//int Circle::numOfCircles = 0;
-//int main(void)
-//{
-//	Circle* p = new Circle[10];
-//	cout << "»ıÁ¸ÇÏ°í ÀÖ´Â ¿øÀÇ °³¼ö = " << Circle::getNumOfCircles() << endl;
-//	delete[]p;
-//	cout << "»ıÁ¸ÇÏ°í ÀÖ´Â ¿øÀÇ °³¼ö = " << Circle::getNumOfCircles() << endl;
-//	Circle a;
-//	cout << "»ıÁ¸ÇÏ°í ÀÖ´Â ¿øÀÇ °³¼ö = " << Circle::getNumOfCircles() << endl;
-//	Circle b;
-//	cout << "»ıÁ¸ÇÏ°í ÀÖ´Â ¿øÀÇ °³¼ö = " << Circle::getNumOfCircles() << endl;
-//}
-//
-//// ÇÔ¼ö¿¡ ¹è¿­ ÀÔ·Â¹Ş±â
-//#include <iostream>
-//using namespace std;
-//int add(int a[], int size, int b[]={})
-//{
-//	int asum = 0;
-//	int bsum = 0;
-//	for (int i = 0; i < size; i++)
-//	{
-//		asum += a[i];
-//		bsum += b[i];
-//	}
-//	return asum + bsum;
-//}
-//int main(void)
-//{
-//	int a[] = { 1,2,3,4,5 };
-//	int b[] = { 6,7,8,9,10 };
-//	int c = add(a, 5);
-//	int d = add(a, 5, b);
-//	cout << c << endl;
-//	cout << d << endl;
-//}
-//
-//// Å¬·¡½º¿¡ °ª, ¹®ÀÚ¿­ ÀÔ·Â
-//#include <iostream>
-//using namespace std;
-//class Person
-//{
-//	int id;
-//	double weight;
-//	string name;
-//public:
-//	Person(int id=1, string name=" ", double weight=20.5)
-//	{
-//		this->id = id;
-//		this->name = name;
-//		this->weight = weight;
-//	}
-//	void show()
-//	{
-//		cout << id << " " << weight << " " << name << endl;
-//	}
-//};
-//int main(void)
-//{
-//	Person grace, ashley(2, "Ashley"), helen(3, "Helen", 32.5);
-//	grace.show();
-//	ashley.show();
-//	helen.show();
-//}
-//
-//// °ª ÀÔ·Â ¾ÈÇÒ½Ã ÀÓÀÇ·Î °ª ÁöÁ¤
-//#include <iostream>
-//using namespace std;
-//int big(int a=100,int b=100,int c=100)
-//{
-//	if (a > b)
-//	{
-//		if (a > c)
-//			return c;
-//		else
-//			return a;
-//	}
-//	else
-//	{
-//		if (b < c)
-//			return b;
-//		else
-//			return c;
-//	}
-//}
-//int main(void)
-//{
-//	int x = big(3, 5);
-//	int y = big(300, 60);
-//	int z = big(30, 60, 50);
-//	cout << x << " " << y << " " << z << endl;
-//}
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	// string í˜•íƒœë¡œ ë°°ì—´ ìƒì„±í•˜ì—¬ ë¬¸ì íƒìƒ‰
+	string names[5];
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "ì´ë¦„ >> ";
+		getline(cin, names[i], '\n');
+	}
+	string first = names[0];
+	string latter = names[0];
+	for (int i = 1; i < 5; i++)
+	{
+		if (latter < names[i])
+		{
+			latter = names[i];
+		}
+		if (first > names[i])
+		{
+			first = names[i];
+		}
+	}
+	cout << "ì‚¬ì „ì—ì„œ ê°€ì¥ ë’¤ì— ë‚˜ì˜¤ëŠ” ë¬¸ìì—´ì€ " << latter << endl;
+	cout << "ì‚¬ì „ì—ì„œ ê°€ì¥ ì•ì— ë‚˜ì˜¤ëŠ” ë¬¸ìì—´ì€ " << first << endl;
+
+	// ì˜ì–´ë¡œ ë¬¸ìì—´ì„ ì…ë ¥ë°›ì•„ ìë¦¬ë¥¼ ì°¾ì•„ ì¶œë ¥
+	string s;
+	cout << "ë¬¸ìì—´ì„ ì…ë ¥í•˜ì‹œì˜¤(í•œê¸€ ì•ˆë¨)" << endl;
+	getline(cin, s, '\n');
+	int len = s.length();
+	for (int i = 0; i < len; i++)
+	{
+		string first = s.substr(0, 1);
+		string sub = s.substr(1, len - 1);
+		s = sub + first;
+		cout << s << endl;
+	}
+}
+
+
+// í•¨ìˆ˜ ì¤‘ë³µí•˜ì—¬ ì‚¬ìš© ê°€ëŠ¥
+#include <iostream>
+using namespace std;
+int big(int a, int b)
+{
+	if (a > b) return a;
+	else return b;
+}
+int big(int* p, int size)
+{
+	int res = *p;
+	for (int i = 1; i < size; i++)
+		if (res < *(p + i)) res = *(p + i);
+	return res;
+}
+int main(void)
+{
+	int array[5] = { 1,9,-2,8,6 };
+	cout << big(2, 3) << endl;
+	cout << big(array, 5) << endl;
+}
+
+// ì›í•˜ëŠ” ê°’ì„ ì…ë ¥ ì•ˆí• ì‹œ ë‹¤ë¥¸ ì •í•´ì§„ ê°’ìœ¼ë¡œ ì§€ì •
+#include <iostream>
+using namespace std;
+void star(int a = 5)
+{
+	for (int i = 0; i < a; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+}
+void msg(int id, string text = "")
+{
+	cout << id << " " << text << endl;
+}
+int main(void)
+{
+	star();
+	star(10);
+	msg(10);
+	msg(10, "Hello");
+}
+
+#include <iostream>
+using namespace std;
+void f(char c = ' ', int line = 1)
+{
+	for (int i = 0; i < line; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << c;
+		}
+		cout << endl;
+	}
+}
+int main(void)
+{
+	f();
+	f('%');
+	f('@', 5);
+}
+
+// í´ë˜ìŠ¤ ë©¤ë²„ ê°’ ì§€ì •
+#include <iostream>
+using namespace std;
+class Person
+{
+public:
+	double money;
+	void addMoney(int money)
+	{
+		this->money += money;
+	}
+	static int sharedMoney;
+	static void addShared(int n)
+	{
+		sharedMoney += n;
+	}
+};
+int Person::sharedMoney = 10;
+int main(void)
+{
+	Person han;
+	han.money = 100;
+	han.sharedMoney = 200;
+	Person lee;
+	lee.money = 150;
+	lee.addMoney(200);
+	lee.addShared(200);
+	cout << han.money << " " << lee.money << endl;
+	cout << han.sharedMoney << " " << lee.sharedMoney << " " << Person::sharedMoney << endl;
+}
+
+// ì†Œë©¸ì í™œìš©
+#include <iostream>
+using namespace std;
+class Circle
+{
+private:
+	static int numOfCircles;
+	int radius;
+public:
+	Circle(int r = 1);
+	~Circle() { numOfCircles--; }
+	double getArea() { return 3.14 * radius * radius; }
+	static int getNumOfCircles() { return numOfCircles; }
+};
+Circle::Circle(int r)
+{
+	radius = r;
+	numOfCircles++;
+}
+int Circle::numOfCircles = 0;
+int main(void)
+{
+	Circle* p = new Circle[10];
+	cout << "ìƒì¡´í•˜ê³  ìˆëŠ” ì›ì˜ ê°œìˆ˜ = " << Circle::getNumOfCircles() << endl;
+	delete[]p;
+	cout << "ìƒì¡´í•˜ê³  ìˆëŠ” ì›ì˜ ê°œìˆ˜ = " << Circle::getNumOfCircles() << endl;
+	Circle a;
+	cout << "ìƒì¡´í•˜ê³  ìˆëŠ” ì›ì˜ ê°œìˆ˜ = " << Circle::getNumOfCircles() << endl;
+	Circle b;
+	cout << "ìƒì¡´í•˜ê³  ìˆëŠ” ì›ì˜ ê°œìˆ˜ = " << Circle::getNumOfCircles() << endl;
+}
+
+// í•¨ìˆ˜ì— ë°°ì—´ ì…ë ¥ë°›ê¸°
+#include <iostream>
+using namespace std;
+int add(int a[], int size, int b[]={})
+{
+	int asum = 0;
+	int bsum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		asum += a[i];
+		bsum += b[i];
+	}
+	return asum + bsum;
+}
+int main(void)
+{
+	int a[] = { 1,2,3,4,5 };
+	int b[] = { 6,7,8,9,10 };
+	int c = add(a, 5);
+	int d = add(a, 5, b);
+	cout << c << endl;
+	cout << d << endl;
+}
+
+// í´ë˜ìŠ¤ì— ê°’, ë¬¸ìì—´ ì…ë ¥
+#include <iostream>
+using namespace std;
+class Person
+{
+	int id;
+	double weight;
+	string name;
+public:
+	Person(int id=1, string name=" ", double weight=20.5)
+	{
+		this->id = id;
+		this->name = name;
+		this->weight = weight;
+	}
+	void show()
+	{
+		cout << id << " " << weight << " " << name << endl;
+	}
+};
+int main(void)
+{
+	Person grace, ashley(2, "Ashley"), helen(3, "Helen", 32.5);
+	grace.show();
+	ashley.show();
+	helen.show();
+}
+
+// ê°’ ì…ë ¥ ì•ˆí• ì‹œ ì„ì˜ë¡œ ê°’ ì§€ì •
+#include <iostream>
+using namespace std;
+int big(int a=100,int b=100,int c=100)
+{
+	if (a > b)
+	{
+		if (a > c)
+			return c;
+		else
+			return a;
+	}
+	else
+	{
+		if (b < c)
+			return b;
+		else
+			return c;
+	}
+}
+int main(void)
+{
+	int x = big(3, 5);
+	int y = big(300, 60);
+	int z = big(30, 60, 50);
+	cout << x << " " << y << " " << z << endl;
+}
